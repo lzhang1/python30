40b9e8c436b814710f0d8538044046d2369ea2e3
rayzhang <lzhang1>
2015-10-29 11:12:16 +0800

    update arch

---
+++ sample/first.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def normalize(name):
+    return name.capitalize()
+
+if __name__ == "__main__":
+    L1 = ["adam","LISA","BAr"]
+    L2 = list(map(normalize, L1))
+    print (L1)
+    print (L2)
